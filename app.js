47710dd0324d48f1fbdbb0d775a3033244c7b158
Katie King <kingofkaties@gmail.com>
2020-06-09 17:36:10 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// PSEUDOCODE
+
+// user inputs pokemon index #
+
+// pokemon index # used to pull iniformation from API and display inside pokedex viewport
+// click events for number pad allowing user to input pokemon id #
+// user sees number inputs in pokedex finder
+// click "enter" submit button
+// grabs pokemon information based on id # from number pad and finds pokemon with that id from API
+// displays: pokemon image, name, height, weight, type
+
+console.log("hi!");
